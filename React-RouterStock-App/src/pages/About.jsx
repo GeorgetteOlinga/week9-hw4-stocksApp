13c98189be86ba32eb3c1ad8929f6387ed152a35
git <olingageorgette1@gmail.com>
2024-01-10 01:29:49 -0500

    first

---
+++ React-RouterStock-App/src/pages/About.jsx
@@ -0,0 +1,11 @@
+function About() {
+    return (
+      <>
+      <br></br>
+      <br></br>
+      <div>This is About page</div>
+      </>
+    )
+  }
+  
+  export default About
---
+++ React-RouterStock-App/src/pages/Home.jsx
@@ -0,0 +1,14 @@
+
+function Home() {
+    return (
+      <>
+      <br></br>
+      <br></br>
+     
+  
+      <div>This is the Home Page</div>
+      </>
+    )
+  }
+  
+  export default Home
